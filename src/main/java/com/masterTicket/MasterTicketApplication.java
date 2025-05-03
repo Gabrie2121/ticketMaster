49444a39978feb19f0c27650ab11e81de9d73c01
GabrielMirandaBarboza <gabriel.barboza@fi-group.com>
2025-05-03 00:41:49 -0300

    Projeto Inicializado

---
+++ src/main/java/com/masterTicket/MasterTicketApplication.java
@@ -0,0 +1,13 @@
+package com.masterTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterTicketApplication.class, args);
+	}
+
+}
